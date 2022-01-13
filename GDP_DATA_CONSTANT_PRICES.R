8dfa373a525544ba3fd3d3451fc7fb55ecb7c124
cranade <chanderranade@gmail.com>
2022-01-12 20:47:26 -0500

    BRICS_CONSTANT_PRICES_DGP

---
+++ GDP_DATA_CONSTANT_PRICES.R
@@ -0,0 +1,2 @@
+gdp_data <- read_excel("C:/Users/Chandra/OneDrive/Desktop/COVID_BRICS/BRICS_US_Constant_Prices.xlsx")
+gdp_data <- read_excel("C:/Users/Chandra/OneDrive/Desktop/COVID_BRICS/BRICS_US_Constant_Prices.xlsx")
