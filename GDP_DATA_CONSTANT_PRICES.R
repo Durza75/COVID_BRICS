7c73ebfc94b27fb080a08004f5ccd61c3a1f6a3d
Chandra Ranade <88807641+cranade@users.noreply.github.com>
2022-01-31 20:02:57 -0500

    Delete GDP_DATA_CONSTANT_PRICES.R

--- GDP_DATA_CONSTANT_PRICES.R
+++
@@ -1,2 +0,0 @@
-gdp_data <- read_excel("C:/Users/Chandra/OneDrive/Desktop/COVID_BRICS/BRICS_US_Constant_Prices.xlsx")
-gdp_data <- read_excel("C:/Users/Chandra/OneDrive/Desktop/COVID_BRICS/BRICS_US_Constant_Prices.xlsx")
